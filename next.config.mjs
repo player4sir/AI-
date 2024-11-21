db163a301eb23580826cafbec4e27e0d37278560
unknown <player4sir@gmail.com>
2024-11-21 21:03:44 +0800

    禁用eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // 在构建时忽略 ESLint 错误
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
